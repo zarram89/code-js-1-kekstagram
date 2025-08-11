803cac41c2964b8324b5fe67443dc6ef45d4861f
Ramil <zarram89@gmail.com>
2025-08-11 12:33:35 +0500

    ауfeat/добавялет валдаÑидацию

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {renderPictures} from './pictures.js';
 import {generateSimilarPictures} from './data.js';
+import './form.js';
 
 const picturesContainer = document.querySelector('.pictures');
 renderPictures(generateSimilarPictures(), picturesContainer);
